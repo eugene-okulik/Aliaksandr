7ca03b5d80d8904064ca09a6ebd04e57b2b5fc1b
Aliaksandr_Tarfimau_2 <trafalex91@gmail.com>
2025-01-26 18:58:57 +0300

    homework_3_done

---
+++ homework/aliaksandr_trafimau/Homework_3/homework_3.1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the number x: "))
+b = int(input("Enter the number y: "))
+
+Sum = a + b
+difference = a - b
+multiply = a * b
+
+print ("Sum", Sum)
+print ("Diffrence", difference)
+print("Multiply", multiply)
---
+++ homework/aliaksandr_trafimau/Homework_3/homework_3.3.py
@@ -0,0 +1,9 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+arithmetic_mean = (a + b) / 2
+
+geometric_mean = (a * b) ** 0.5
+
+print("Arithmetic Mean:", arithmetic_mean)
+print("Geometric Mean:", geometric_mean)
