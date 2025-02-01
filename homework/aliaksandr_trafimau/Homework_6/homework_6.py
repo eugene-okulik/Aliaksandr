2457f1557d21fdab3950969aa64e3143b7d81047
Aliaksandr_Tarfimau_2 <trafalex91@gmail.com>
2025-02-01 22:16:52 +0300

    homework_6_done

---
+++ homework/aliaksandr_trafimau/Homework_6/homework_6.py
@@ -0,0 +1,5 @@
+a = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+words = a.replace(',', '').replace('.', '').split()
+b = [word + 'ing' for word in words]
+c = ' '.join(b)
+print(c)
