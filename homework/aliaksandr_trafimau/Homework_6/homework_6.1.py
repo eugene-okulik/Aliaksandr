58decd415eb32bee9d00bd447a86ba08e20a9072
Aliaksandr_Tarfimau_2 <trafalex91@gmail.com>
2025-02-01 22:33:06 +0300

    homework_6_updated

--- homework/aliaksandr_trafimau/Homework_6/homework_6.1.py
+++ homework/aliaksandr_trafimau/Homework_6/homework_6.1.py
@@ -1,11 +1,11 @@
-number = list(range(1, 101))
-for at in number:
-    if at % 3 == 0 and at % 5 == 0:
+numbers = list(range(1, 101))
+for num in numbers:
+    if num % 3 == 0 and num % 5 == 0:
         print('FuzzBuzz')
-    elif at % 3 == 0:
+    elif num % 3 == 0:
         print('Fuzz')
-    elif at % 5 == 0:
+    elif num % 5 == 0:
         print('Buzz')
     else:
-        print(at)
+        print(num)
         
