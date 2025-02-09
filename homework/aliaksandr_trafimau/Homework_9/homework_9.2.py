4fec6c81cdad51ae086238877c8b509ffcf4d9e7
Aliaksandr_Tarfimau_2 <trafalex91@gmail.com>
2025-02-09 18:21:36 +0300

    homewrok_9_update

--- homework/aliaksandr_trafimau/Homework_9/homework_9.2.py
+++ homework/aliaksandr_trafimau/Homework_9/homework_9.2.py
@@ -1,13 +1,15 @@
 import statistics
 
-temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32,
-                30, 28, 24, 23]
+temperatures = [
+    20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29,
+    29, 31, 33, 31, 30, 32, 30, 28, 24, 23
+]
 
-def hot_temperature(x):
+def is_hot_temperature(x):
     return x > 28
 
-list_hot_temperatures = list(filter(hot_temperature, temperatures))
+hot_temperatures = list(filter(is_hot_temperature, temperatures))
 
-print(list_hot_temperatures)
-print(max(list_hot_temperatures))
-print(statistics.median(list_hot_temperatures))
+print("Горячие температуры:", hot_temperatures)
+print("Максимальная температура:", max(hot_temperatures))
+print("Медиана горячих температур:", statistics.median(hot_temperatures))
